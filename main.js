f1e91929c70713f00e3d7a8f09547c7a8c79cd4a
Mark Hamilton <ham.mark126@gmail.com>
2022-10-22 13:55:38 -0500

    added sorting hat card, sort form, and  renderToDom

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("We're sorting!");
+
+const students = [];
+
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToRender;
+};
+
+const sortButton = document.getElementById("start-sorting");
+
+sortButton.addEventListener('click', () => {
+  const form = document.getElementById('form');
+}); 
