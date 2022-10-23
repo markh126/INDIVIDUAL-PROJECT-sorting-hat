7cc566819b9b465a51bc942f2fac8d928481b6df
Mark Hamilton <ham.mark126@gmail.com>
2022-10-23 15:33:28 -0500

    created dynamic student form

--- main.js
+++ main.js
@@ -7,8 +7,23 @@ const renderToDom = (divId, htmlToRender) => {
   selectedDiv.innerHTML = htmlToRender;
 };
 
-const sortButton = document.getElementById("start-sorting");
+const formOnDom = () => {
+  let domString = "";
+  domString += `<form>
+  <div class="mb-3" id="sort-form">
+    <h5 class="form-title">Enter First Year's Name</h5>
+    <label for="student-name" class="form-label" id="name">Student Name</label>
+    <input type="name" class="form-control" id="first-year-name" placeholder="Harry Potter">
+    <button type="submit" class="btn btn-primary" id="sort">Sort!</button>
+  </div>
+</form>`
 
-sortButton.addEventListener('click', () => {
-  const form = document.getElementById('form');
-}); 
+renderToDom("#app", domString);
+}
+
+
+const showFormButton = document.querySelector("#start-sorting");
+
+showFormButton.addEventListener('click', () => {
+  formOnDom();
+})
