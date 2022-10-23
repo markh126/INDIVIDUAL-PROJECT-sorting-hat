d54a2b1516c6fe9ca7599e73702684b31c3340f3
Mark Hamilton <ham.mark126@gmail.com>
2022-10-23 16:36:13 -0500

    working on adding student cards

--- main.js
+++ main.js
@@ -27,3 +27,27 @@ const showFormButton = document.querySelector("#start-sorting");
 showFormButton.addEventListener('click', () => {
   formOnDom();
 })
+
+const cardsOnDom = () => {
+  let domString2 = "";
+  domString2 += `<div class="card mb-3" style="max-width: 480px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Student Name</h5>
+        <p class="card-text">House Name</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>`
+
+renderToDom("#app", domString2);
+}
+
+const showStudentButton = document.querySelector("#sort");
+
+showStudentButton.addEventListener("submit",);
